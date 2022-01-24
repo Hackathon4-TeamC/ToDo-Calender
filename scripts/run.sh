8d12f14c834ebcaa936e0c23a9d54a19dbffb63f
Junichi Nishi <github@purewest.dip.jp>
2022-01-25 01:23:42 +0900

    migrateを自動化

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,7 +6,7 @@ do
     sleep 1
 done
 
-# cd /usr/src/app/db && alembic upgrade head
+cd /usr/src/app/db && alembic upgrade head
 # cd /usr/src/app/db && python seed.py
 
 cd /usr/src/app/app && uvicorn main:app --reload --port=8000 --host=0.0.0.0
