a53f59a5c5e2bfea2abbe542b7b90d85afeb71fe
sari-1999 <ya3de45c72yurc@gmail.com>
2022-02-17 21:10:57 +0900

    SignUp(仮)

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,8 +1,11 @@
+
+   
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { DevLink } from "../components/pages/DevLink/DevLink";
 import { Home } from "../components/pages/Home/Home";
 import { SignIn } from "../components/pages/SignIn/SignIn";
+import {SignUp} from "../components/pages/SignUp/SignUp"
 
 export const Router = () => {
   return (
@@ -11,6 +14,7 @@ export const Router = () => {
         <Route path="/" element={<DevLink />} />
         <Route path="/SignIn" element={<SignIn />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/signup element={<SignUp/>} />
       </Routes>
     </>
   );
