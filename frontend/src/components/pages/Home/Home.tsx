048ecf83a5c202d8b1b577844c5817232f2bc112
haru-24 <h24wler@gmail.com>
2022-02-22 23:15:14 +0900

    Add : axios mock

--- frontend/src/components/pages/Home/Home.tsx
+++ frontend/src/components/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
-import { memo, VFC } from "react";
+import { memo, useEffect, VFC } from "react";
 import styles from "./Home.module.css";
 import { CalenderSidebar } from "../../organisms/CalenderSidebar/CalenderSidebar";
+import { getUsers } from "../../../api/getUser";
 
 export const Home: VFC = memo(() => {
+  // test🔥🔥🔥🔥🔥🔥🔥🔥
+  useEffect(() => {
+    getUsers()
+      .then((result) => {
+        console.log("# users", result);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
   return (
     <>
       <CalenderSidebar />
