b6cc10763f5f22a5b8b04267f66ca10f0bcb0b2e
haru-24 <h24wler@gmail.com>
2022-03-02 10:33:47 +0900

    Improve : combine sidebar and calendar

--- frontend/src/components/pages/Calendar/Calendar.tsx
+++ frontend/src/components/pages/Calendar/Calendar.tsx
@@ -1,19 +1,34 @@
-import { memo, VFC } from "react";
+import { memo, useState, VFC } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from '@fullcalendar/interaction';
-
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import { SlideInBottombar } from "../../organisms/SlideInBottombar";
 
 export const Calendar: VFC = memo(() => {
-    
-    return (
-        <div>
-            <FullCalendar
-                plugins={[dayGridPlugin, interactionPlugin]}
-                initialView="dayGridMonth"
-                selectable={true}
-                contentHeight='auto'
-            />
-        </div>  
-    )
-})
+  // state
+  const [visible, setVisible] = useState(false);
+  const [dateStr, setDate] = useState<string>();
+
+  const dateClick = (info: DateClickArg) => {
+    console.log("クリックしたよ", info.dateStr);
+    setVisible(true);
+    setDate(info.dateStr);
+  };
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        initialView="dayGridMonth"
+        selectable={true}
+        contentHeight={"90vh"}
+        dateClick={dateClick}
+      />
+
+      <SlideInBottombar
+        visible={visible}
+        setVisible={setVisible}
+        dateStr={dateStr}
+      />
+    </div>
+  );
+});
