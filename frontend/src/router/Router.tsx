1cd758c38984b17a0d28164bf6bd50f53e1097b7
haru-24 <h24wler@gmail.com>
2022-03-02 15:26:48 +0900

    Fix : change directory structure

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "../components/pages/Home/Home";
 import { SignIn } from "../components/pages/SignIn/SignIn";
-import { SlideIn } from "../components/pages/SlideIn/SlideIn";
 import { SignUp } from "../components/pages/SignUp/SignUp";
-import { Calendar } from "../components/pages/Calendar/Calendar";
 
 export const Router = () => {
   return (
@@ -12,9 +10,7 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
-        <Route path="/slidein" element={<SlideIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </>
   );
