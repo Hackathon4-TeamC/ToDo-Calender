eeadc459a8577605e4daad200d87ffa488b55abf
haru-24 <h24wler@gmail.com>
2022-03-04 14:05:37 +0900

    Add : 404 NotFound page

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import { RouteAuthGuard } from "../components/Auth/RouteAuthGuard";
 import { Home } from "../components/pages/Home/Home";
+import { NotFound } from "../components/pages/NotFound";
 import { SignIn } from "../components/pages/SignIn/SignIn";
 import { SignUp } from "../components/pages/SignUp/SignUp";
 
@@ -8,9 +10,13 @@ export const Router = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<RouteAuthGuard component={<Home />} redirect="/signin" />}
+        />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </>
   );
